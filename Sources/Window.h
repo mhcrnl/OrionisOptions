f805d9d68872be08ca616e6ca8bd61d369d89898
Patrick Jahnig <psj516@vt.edu>
2017-05-18 22:17:10 -0400

    Added string constants to replace various hard-coded strings in Window.c

--- Sources/Window.h
+++ Sources/Window.h
@@ -1,10 +1,19 @@
 #include <gtk/gtk.h>
 
-static GtkWidget *aboutItem, *audioBox, *fileItem, *fileMenu, *graphicsBox, *menuBar, *menuBox, *optionsBox, *optionsNotebook, *quitItem, *saveButton, *window, *windowBox;
-static GtkWidget *musicRange, *sfxRange;
-static GtkWidget *musicFrame, *sfxFrame;
-static GtkWidget *resolutionChoices, *windowChoices;
-static GtkWidget *resolutionFrame, *windowFrame;
+static const char   *about          = "Orionis Options is a simple external options application for Orionis, designed to help you change settings without having to open the game first or manually edit a file.";
+static const char   *name           = "Orionis | Options";
+static const char   *settingsFormat = "[Settings]\nMusic Volume: %u\nSFX Volume: %u\nResolution: %u\nDisplay: %u";
+static const char   *website        = "https://aerodlyn.github.io/";
+
+static const char   *authors []     = { "Patrick Jahnig" };
+static const char   *displays []    = { "Fullscreen", "Borderless", "Bordered" };
+static const char   *resolutions [] = { "1366 x 768", "1600 x 900", "1920 x 1080" };
+
+static GtkWidget    *aboutItem, *audioBox, *fileItem, *fileMenu, *graphicsBox, *menuBar, *menuBox, *optionsBox, *optionsNotebook, *quitItem, *saveButton, *window, *windowBox;
+static GtkWidget    *musicRange, *sfxRange;
+static GtkWidget    *musicFrame, *sfxFrame;
+static GtkWidget    *resolutionChoices, *windowChoices;
+static GtkWidget    *resolutionFrame, *windowFrame;
 
 void initGUI ();
 
