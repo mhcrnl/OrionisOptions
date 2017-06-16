bc2720cd5bc21c11e7fbf245c5278874f3584e38
Patrick Jahnig <psj516@vt.edu>
2017-06-16 17:51:12 -0400

    Removed to replace with makefiles.

--- Compile.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Compile and execute Orionis Options
-# NOTE: This script must be in the root directory of the Sources folder (i.e. ../Sources)
-
-gcc -o OrionisOptions -Wall Sources/*.c `pkg-config --libs --cflags gtk+-3.0` && ./OrionisOptions && clear
